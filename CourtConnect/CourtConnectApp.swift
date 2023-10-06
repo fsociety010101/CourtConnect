caf370c0a4d0e93b1869dabeae8138aeaa74e4d3
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-06 18:45:22 +0200

    Initial Commit

---
+++ CourtConnect/CourtConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtConnectApp.swift
+//  CourtConnect
+//
+//  Created by Dmytro Nimchynskyi on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CourtConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
